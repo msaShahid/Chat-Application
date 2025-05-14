5c3e7e0826f32c0fd7f31678d08083a8140e7125
Mohammad Shahid <msa.ansari28@gmail.com>
2025-05-14 17:32:59 +0530

    `Added script tag for app.js in client/index.html`

---
+++ client/app.js
@@ -0,0 +1,19 @@
+const socket = new WebSocket("ws://localhost:3001");
+
+function sendMessage() {
+    const input = document.getElementById('messageInput');
+    const message = input.value.trim();
+    if (message) {
+    socket.send(message);
+    addMessage(`🧑 You: ${message}`);
+    input.value = '';
+    }
+}
+
+function addMessage(text) {
+      const messagesDiv = document.getElementById('messages');
+      const msg = document.createElement('div');
+      msg.textContent = text;
+      messagesDiv.appendChild(msg);
+      messagesDiv.scrollTop = messagesDiv.scrollHeight;
+}
