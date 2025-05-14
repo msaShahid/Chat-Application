68aec2aae225656d7e0bb591f90743776b51fdcc
Mohammad Shahid <msa.ansari28@gmail.com>
2025-05-14 17:25:08 +0530

    `Update server/app.js and package.json to reflect changes in WebSocket server setup and script execution.`

--- server/app.js
+++ server/app.js
@@ -5,6 +5,7 @@ const webServer = new ws.WebSocketServer({
 });
 
 webServer.on("connection", (socket) => {
+    console.log('Connection is running');
     socket.on("message", () =>{
         console.log(`Received message: ${message}`);
     })
