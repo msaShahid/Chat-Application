7781faed6e66b3b1c6d4276390e7e15d95b9dcf4
Mohammad Shahid <msa.ansari28@gmail.com>
2025-05-15 15:43:56 +0530

    `Removed client and server code for WebSocket chat application.`

--- server/app.js
+++ server/app.js
@@ -16,29 +16,7 @@ const io = new  Server(httpServer, {
 });
 
 io.on("connection", (socket) => {
-   // console.log(`User ${socket.id} connected`);
 
-    // At connection - only to current user
-    socket.emit("message", "Welcome to the chat");
-
-    // At connectio - to all user
-    socket.broadcast.emit("message", `User ${socket.id.substring(0, 5)} connected`)
-
-    // When Disconnection 
-    socket.on('disconnect', () =>{
-        socket.broadcast.emit("message", `User ${socket.id.substring(0, 5)} disconnected`)
-    })
-   
-    // capturing the activity event
-    socket.on("activity", (name) => {
-        console.log(name);
-        socket.broadcast.emit("activity", name);
-    })
-
-    socket.on("message", (data) =>{
-        console.log(`Received message from client: ${data}`);
-        io.emit("message",`${socket.id.substring(0, 5)} : ${data}`);
-    })
 })
 
 
